0c41895074678add92218b84eb425a3e73e33706
matias23 <matiaspedraza023@gmail.com>
2023-03-22 00:09:06 -0300

    agregando mensaje a index

--- Curso-Javascript/index.js
+++ Curso-Javascript/index.js
@@ -1,2 +1,2 @@
-console.log("Esta es la puerta de entrada al proyecto");
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo.");
 //este es el mensaje de bienvenida
